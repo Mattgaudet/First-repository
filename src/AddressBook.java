06dbb1d13990c1194c8810b195006711a898e592
mattg <matthewgaudet@cmail.carleton.ca>
2022-09-26 14:23:32 -0400

    New documentation added

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,5 +1,9 @@
 import java.util.ArrayList;
+//https://github.com/Mattgaudet/First-repository.git
 
+/**
+ * My new documentation
+ */
 public class AddressBook {
     ArrayList<BuddyInfo> infoList;
 
@@ -7,10 +11,13 @@ public class AddressBook {
         infoList = new ArrayList<>();
     }
     public void addBuddy(BuddyInfo info) {
-        infoList.add(info);
+        if(info != null) {
+            infoList.add(info);
+        }
     }
     public void removeBuddy(BuddyInfo info) {
         infoList.remove(info);
+
     }
     public static void main(String[] args) {
         BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", 613);
